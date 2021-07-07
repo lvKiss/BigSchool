d101eec87eec55f2620d6c322133aeb1239388df
lvKiss <76516074+lvKiss@users.noreply.github.com>
2021-07-07 20:26:11 +0700

    Initial commit

---
+++ Models/BigSchoolContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BigSchool.Models
+{
+    public partial class BigSchoolContext : DbContext
+    {
+        public BigSchoolContext()
+            : base("name=BigSchoolContext")
+        {
+        }
+
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.AspNetUser)
+                .HasForeignKey(e => e.LecturerId);
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,30 @@
+﻿namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(128)]
+        public string LecturerId { get; set; }
+
+        [Required(ErrorMessage ="không để trống")]
+        [StringLength(255)]
+        public string Place { get; set; }
+        [Required(ErrorMessage = "không để trống")]
+        public int? CategoryId { get; set; }
+        public string Name;
+
+        public DateTime? DateTime { get; set; }
+        public AspNetUser AspNetUser { get; internal set; }
+
+        public List<Category> ListCategory = new List<Category>();
+    }
+}
